7e220a2400a11c397a5bb22e66c3ae0010093fda
WantedHorizon <eliran.amzalag@gmail.com>
2020-10-04 16:34:30 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import jumboData from './fixtures/jumbo.json';
+import Jumbotron from './components/jumbotron';
+function App() {
+  return (
+    <div className="App" >
+      <Jumbotron.Container>
+        {jumboData.map((item) => (
+          <Jumbotron
+            key={item.id}
+            direction={item.direction}
+          >
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+
+            <Jumbotron.Pane>
+              <Jumbotron.Image alt={item.alt} src={item.image} />
+            </Jumbotron.Pane>
+
+          </Jumbotron>
+        )
+        )}
+      </Jumbotron.Container>
+    </div>
+  );
+}
+
+export default App;
