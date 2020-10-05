97edf1812a4f22b28c4c9115a487bac309139f3b
WantedHorizon <eliran.amzalag@gmail.com>
2020-10-05 10:41:54 +0300

    accordion faq

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FaqsContainer from './containers/FaqContainer';
 import FooterContainer from './containers/FooterContainer';
 import JumbotronContainer from './containers/JumbotronContainer';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </div>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export { default as Jumbotron } from './jumbotron';
 export { default as Footer } from './footer';
+export { default as Accordion } from './accordion';
