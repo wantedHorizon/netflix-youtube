ca695b90dd6ef829a41c7c03af5d8fe9be5fc4cd
WantedHorizon <eliran.amzalag@gmail.com>
2020-10-09 11:35:28 +0300

    .env fix

--- src/api/youtube-api.js
+++ src/api/youtube-api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// const KEY = 'AIzaSyCEFfEcLTX9ZKIY2UHfOjY4cBEtiJQy6v8';
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
